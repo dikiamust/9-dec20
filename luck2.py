b39249d31b25fc32bd77ef90f357c043bba61992
dikiamust <agungmustaqimdiki@gmail.com>
2020-12-12 14:47:39 +0700

    revisi

--- luck2.py
+++ luck2.py
@@ -1,11 +1,30 @@
 # TIPE DATA DICTIONARY = PERINTAH MENGHUBUNGKAN ANTARA KEY DAN VALUE / KEY VALUE PAIR /KVP
 
 kamus = {}
-kamus ['anak'] = 'son'
-kamus ['ayah'] = 'father'
-kamus ['ibu'] = 'mather'
-kamus ['istri'] = 'wife'
-kamus ['suami'] = 'husband'
-print(kamus ['anak'])
-print(kamus ['ibu'])
+kamus['anak'] = 'son'
+kamus['ayah'] = 'father'
+kamus['ibu'] = 'mother'
+kamus['istri'] = 'wife'
+kamus['suami'] = 'husband'
+print( kamus['anak'] )
+print( kamus['ibu'] )
 
+# STUDY CASE DATA DRIVER YANG BERADA DI SEKITAR (DATA DARI SERVER)
+data_dari_server_gojek = {
+    'tanggal': '2020-12-11',
+    'driver_list': ['eko', 'dwi', 'tri', 'catur','limo']
+}
+print(data_dari_server_gojek)
+print(f" driver kedua {data_dari_server_gojek['driver_list'][1]}")
+
+# STUDY CASE SEDIKIT KOMPLEKS
+data_dari_server_gokar = {
+    'gokar_list': [
+    {'nama': 'eko','jarak': 10},
+    {'nama' : 'dwi', 'jarak' : 20},
+    {'nama' : 'tri', 'jarak' : 30},
+    {'nama' : 'catur', 'jarak' : 40},
+    {'nama' : 'limo', 'jarak' : 50}
+]
+}
+print(f" gokar ke-4 {data_dari_server_gokar ['gokar_list'] [3]}")
