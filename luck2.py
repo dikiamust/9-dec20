94b4258c86cd4f99094280ddbd05be85a5dcfffc
dikiamust <agungmustaqimdiki@gmail.com>
2020-12-12 15:11:09 +0700

    revisi

--- luck2.py
+++ luck2.py
@@ -12,21 +12,24 @@ print( kamus['ibu'] )
 # STUDY CASE DATA DRIVER YANG BERADA DI SEKITAR (DATA DARI SERVER)
 data_dari_server_gojek = {
     'tanggal': '2020-12-11',
-    'driver_list': ['eko', 'dwi', 'tri', 'catur','limo']
+    'driver_list': ['eko', 'dwi', 'tri', 'catur', 'limo']
 }
-print(data_dari_server_gojek)
-print(f" driver kedua {data_dari_server_gojek['driver_list'][1]}")
+print( data_dari_server_gojek )
+print( f" driver kedua {data_dari_server_gojek['driver_list'][1]}" )
 
 # STUDY CASE SEDIKIT KOMPLEKS
 data_dari_server_gokar = {
     'gokar_list': [
-    {'nama': 'eko','jarak': 10},
-    {'nama' : 'dwi', 'jarak' : 20},
-    {'nama' : 'tri', 'jarak' : 30},
-    {'nama' : 'catur', 'jarak' : 40},
-    {'nama' : 'limo', 'jarak' : 50}
-]
+        {'nama': 'eko', 'jarak': 10},
+        {'nama': 'dwi', 'jarak': 20},
+        {'nama': 'tri', 'jarak': 30},
+        {'nama': 'catur', 'jarak': 40},
+        {'nama': 'limo', 'jarak': 50}
+    ]
 }
-print('berikut adalah data gokar :')
-print(f" gokar ke-4 {data_dari_server_gokar ['gokar_list'] [3]}")
-print(f" gokar ke-5 {data_dari_server_gokar ['gokar_list'] [4]}")
+print( 'berikut adalah data gokar :' )
+print( f" gokar ke-4 {data_dari_server_gokar['gokar_list'][3]}" )
+print( f" gokar ke-5 {data_dari_server_gokar['gokar_list'][4]}" )
+print( f" jarak gokar ke-5 : {data_dari_server_gokar['gokar_list'][4] ['jarak']} meter")
+print( f" namanya adalah : {data_dari_server_gokar['gokar_list'][4] ['nama']} ")
+
